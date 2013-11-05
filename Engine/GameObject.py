9625a5570ef633f060ba2b653f5725d2cfb62b78
ben <ben.spoor@gmail.com>
2013-11-05 20:04:22 +0100

    Added license headers

--- Engine/GameObject.py
+++ Engine/GameObject.py
@@ -1,4 +1,32 @@
-""" Base class for a game object """
+"""
+
+    This file is part of HexACO.
+
+    HexACO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HexACO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HexACO.  If not, see <http://www.gnu.org/licenses/>.
+
+########################################################################
+
+ Game Object Class
+ including Unit test class
+
+ * Run file separate to run unit tests
+
+########################################################################
+
+Description
+-----------
+Base class for a game object """
 
 import unittest
 from Engine.HexagonalPosition import HexagonalPosition
