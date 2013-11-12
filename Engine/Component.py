8fa8387850e0399d82b1e9899038c7ec3782b9ff
ben <ben.spoor@gmail.com>
2013-11-12 15:46:28 +0100

    Added component class for Component Based design

---
+++ Engine/Component.py
@@ -0,0 +1,78 @@
+"""
+
+    This file is part of HexACO.
+
+    HexACO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HexACO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HexACO.  If not, see <http://www.gnu.org/licenses/>.
+
+########################################################################
+
+ Component Class
+ including Unit test class
+
+ * Run file separate to run unit tests
+
+########################################################################
+
+Description
+-----------
+Base class for a component """
+
+import unittest
+
+class Component(GameObject):
+    """A component is a base class for features of gameobjects 
+    """
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.initialize()
+
+###################################################################
+#
+# Test Code
+#
+###################################################################
+
+class TestComponent(unittest.TestCase):
+
+    ######################################################
+
+    @classmethod
+    def setUpClass(cls):
+        "This method is called once, when starting the tests"
+        cls.comp = Component(None)
+
+    @classmethod
+    def tearDownClass(cls):
+        "This method is called after finishing all tests"
+        pass
+
+    #######################################################
+
+    def setUp(self):
+        "This method is called befire each test case"
+        pass
+
+    def tearDown(self):
+        "This method is called after each test case"
+        pass
+
+    #######################################################
+
+    def test_defaultName(self):
+
+        self.assertEqual( type( self.comp), Component )
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
