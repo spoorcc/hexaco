1c71a9eb82e66c0a429ae82862d8459bf9cf17a0
ben <ben.spoor@gmail.com>
2013-12-11 22:53:07 +0100

    Reformatted

--- Engine/GameEngine.py
+++ Engine/GameEngine.py
@@ -25,8 +25,9 @@ Description
 -----------
 Base class for a game Engine """
 
-from GameObject import GameObject
-from GameObjectFactory import GameObjectFactory
+from Engine.GameObject import GameObject
+from Engine.GameObjectFactory import GameObjectFactory
+
 
 class GameEngine(object):
     """The engine containing all gameobjects and tiles
@@ -47,65 +48,65 @@ class GameEngine(object):
         self.game_object_factory = GameObjectFactory(self)
         self.callbacks_for_new_object = []
 
-    def callback_for_new_object(self, method_to_call ):
+    def callback_for_new_object(self, method_to_call):
         """ Methods registered here will be called
             when a new object is created """
 
         if callable(method_to_call):
-            self.callbacks_for_new_object.append( method_to_call )
+            self.callbacks_for_new_object.append(method_to_call)
 
-    def set_hex_radius( self, hex_radius ):
-		""" Set the hex radius """
-		self.game_object_factory.hexRadius = hex_radius
+    def set_hex_radius(self, hex_radius):
+        """ Set the hex radius """
+        self.game_object_factory.hex_radius = hex_radius
 
     def initialize_objects(self):
         """ All objects in the world will be initialized here """
 
-        self.create_map( 4 )
+        self.create_map(4)
 
         ant = self.game_object_factory.create_ant()
-        ant.components['position'].pos.set_position_xyz( 0, 0, 0)
-        self.add_game_object( ant )
+        ant.components['position'].pos.set_position_xyz(0, 0, 0)
+        self.add_game_object(ant)
 
     def add_game_object(self, game_object):
         """ Add a game object and call all the methods registered
             to this event with the game_obj handle """
 
         if type(game_object) is GameObject:
-            self.objects[ str(game_object.objectID) ] =  game_object
+            self.objects[str(game_object.objectID)] = game_object
 
             for method in self.callbacks_for_new_object:
-                method( game_object )
+                method(game_object)
 
-    def get_game_object( self, object_id):
+    def get_game_object(self, object_id):
         """ Returns an handle to the game object identified by the
         unique object_id """
-        return self._instance.objects[ str(object_id) ]
+        return self._instance.objects[str(object_id)]
 
     def create_map(self, rings):
         """ Creates a map consisting of a number of rings """
 
         # Create the center tile
         tile_obj = self.game_object_factory.create_tile()
-        self.add_game_object( tile_obj )
+        self.add_game_object(tile_obj)
 
-        for ring in range( rings  ):
-            for side in range( 6 ):  # Hexagon has 6 sides
-                for tile in range( ring ):
+        for ring in range(rings):
+            for side in range(6):  # Hexagon has 6 sides
+                for tile in range(ring):
                     tile_obj = self.game_object_factory.create_tile()
                     pos = tile_obj.components['position'].pos
-                    pos.set_position_rst( ring, side, tile )
+                    pos.set_position_rst(ring, side, tile)
 
-                    self.add_game_object( tile_obj )
+                    self.add_game_object(tile_obj)
 
     def update(self):
         """ Updates all the components in the proper order
         , called as part of the main game loop """
         for obj_id, obj in self.objects.iteritems():
-            self.update_ai( obj )
-            self.update_move( obj )
+            self.update_ai(obj)
+            self.update_move(obj)
 
-    def update_ai(self, obj ):
+    def update_ai(self, obj):
         """ Update all the think actions of each game_object"""
 
         try:
@@ -116,7 +117,7 @@ class GameEngine(object):
 
                 if pos_comp.center_of_tile():
                     print "redirecting"
-                    pos_comp.orientation = (pos_comp.orientation + 1 ) % 6
+                    pos_comp.orientation = (pos_comp.orientation + 1) % 6
 
 
         except:
@@ -134,7 +135,7 @@ class GameEngine(object):
 
                 # Only do the move computations if there is a movement
                 if move_comp.speed != 0.0:
-                    speed_mat = move_comp.get_xyz_speed( pos_comp.orientation )
+                    speed_mat = move_comp.get_xyz_speed(pos_comp.orientation)
 
                     pos_comp.pos.x += speed_mat[0]
                     pos_comp.pos.y += speed_mat[1]
